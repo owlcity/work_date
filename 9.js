763e271474b3e030185476adb983621d916e87a0
zk <596718356@qq.com>
2022-10-20 14:21:26 +0800

    修复上月考勤

---
+++ 9.js
@@ -0,0 +1,152 @@
+let arr_9 = [
+  {
+    date: "2022-09-01",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-02",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-03",
+    type: 1,
+    remark: "周末",
+  },
+  {
+    date: "2022-09-04",
+    type: 1,
+    remark: "周末",
+  },
+  {
+    date: "2022-09-05",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-06",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-07",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-08",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-09",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-10",
+    type: 2,
+    remark: "中秋节",
+  },
+  {
+    date: "2022-09-11",
+    type: 2,
+    remark: "中秋节",
+  },
+  {
+    date: "2022-09-12",
+    type: 2,
+    remark: "中秋节",
+  },
+  {
+    date: "2022-09-13",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-14",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-15",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-16",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-17",
+    type: 1,
+    remark: "周末",
+  },
+  {
+    date: "2022-09-18",
+    type: 1,
+    remark: "周末",
+  },
+  {
+    date: "2022-09-19",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-20",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-21",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-22",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-23",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-24",
+    type: 1,
+    remark: "周末",
+  },
+  {
+    date: "2022-09-25",
+    type: 1,
+    remark: "周末",
+  },
+  {
+    date: "2022-09-26",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-27",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-28",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-29",
+    type: 0,
+    remark: null,
+  },
+  {
+    date: "2022-09-30",
+    type: 0,
+    remark: null,
+  },
+];
